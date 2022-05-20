371f438ee4838022e2f6e5b3095fb999a9f20a5c
Fabian <fabian.sepulveda.s@usach.cl>
2022-05-19 21:05:12 -0400

    ..

--- actividad Regresion/RLS.R
+++ actividad Regresion/RLS.R
@@ -1,1 +1,14 @@
-opción
+#ópcíón
+library(ggpubr)
+
+ingreso <- c(4250,8500,12800,19800,25500)
+cmp <- c(4.12,4.19,4.35,4.66,5.26)
+datos <- data.frame(ingreso,cmp)
+modelo <- lm(cmp ~ ingreso, data = datos)
+print(summary(modelo))
+
+p <- ggscatter(datos, x = "ingreso", y = "cmp", color = "blue", fill = "blue",
+               xlab = "USD/año", ylab = "%")
+p <- p + geom_smooth(method = lm, se = FALSE, colour = "red")
+print(p)
+plot(modelo)
