fd2754b00c8f31354b8169b67abca4cb9c127aa5
Fabian sepulveda <fabian.sepulveda.s@usach.cl>
2022-06-17 15:32:26 -0400

    Termino de comentarios

--- EP14/EP14.R
+++ EP14/EP14.R
@@ -1,8 +1,11 @@
+# LIBRERIAS
 library(dplyr)
 library(car)
 library(mlogit)
 library(pROC)
-#-----------------Funciones-----------------#
+
+#----------------- Funciones -----------------#
+
 #Función que según el IMC indica si esta sobrepeso o no. 
 determinarEN <- function(i, dato){
   if(dato[i] >= 25){
@@ -13,7 +16,11 @@ determinarEN <- function(i, dato){
   }
 }
 
-#-----------------Desarrollo-----------------#
+
+#----------------- Desarrollo -----------------#
+
+
+# Consideramos un alfa = 0.05
 
 #Se lee la información desde el archivo csv entregado.
 basename <- "EP13 Datos.csv"
@@ -43,36 +50,45 @@ hombres.sobrepeso <- sample_n(datos%>%filter(Gender == 1 & EN == 1), 60)
 hombres.nosobrepeso <- sample_n(datos%>%filter(Gender == 1 & EN == 0), 60)
 
 #Se define las muestras de entrenamiento y prueba
+# Junto a ello se realiza un sample para reordenar las variables
 muestra.entrenamiento <- rbind(hombres.nosobrepeso[1:40, ],hombres.sobrepeso[1:40, ])
 muestra.entrenamiento <- sample_n(muestra.entrenamiento, nrow(muestra.entrenamiento))
 muestra.prueba <- rbind(hombres.sobrepeso[41:60, ], hombres.nosobrepeso[41:60, ])
 muestra.prueba <- sample_n(muestra.prueba, nrow(muestra.prueba))
 
+
 #Recordando las 8 variables del trabajo anterior
 
-#Knee.Girth, Biiliac.diameter, Thigh.Girth, Height, Calf.Maximum.Girth, Navel.Girth, Knees.diameter, 
-#Forearm.Girth, Waist.Girth
+
+# "Knee.Girth"  "Biiliac.diameter"  "Thigh.Girth"  "Height"  "Calf.Maximum.Girth"  "Navel.Girth"  "Knees.diameter" 
+# "Forearm.Girth"  "Waist.Girth"
+
 
 variables <- c("Knee.Girth", "Biiliac.diameter", "Thigh.Girth", "Calf.Maximum.Girth",
               "Navel.Girth", "Knees.diameter", "Forearm.Girth")
+
 
 #Se selecciona la variable Waist.Girth
 #Debido a que el estado nutricional hace referencia a si una persona esta en sobrepeso o no,
 # y se espera que una persona que tenga sobrepeso también sea más ancha a la altura de la cintura
 # comparada a una persona sin sobrepeso.
 
+
 #Ajuste del modelo de regresión logística
 modelo <- glm(EN ~ Waist.Girth, family = binomial(link = "logit"), data = muestra.entrenamiento)
 print(summary(modelo))
 
+#Se agrega la variable seleccionada
 variables <- c(variables, "Waist.Girth")
 fstr <- paste("EN", paste(variables, collapse = " + "), sep = " ~ ")
-#SE transforma el string en una fórmula, para ser usado en el modelo ajustado.
+#Se transforma el string en una fórmula, para ser usado en el modelo ajustado.
 formula_m <- formula(fstr)
 
+#Se crea el modelo completo
 modelo.completo <- glm(formula_m, family = binomial(link = "logit"), data = muestra.entrenamiento)
 
-
+#En base a las variables se crea el nuevo modelo 
+#Utilizando la función step para la seleccion de los mejores predictores
 nuevo.modelo <- step(modelo, scope = list(lower = modelo, upper = modelo.completo), direction = "forward", trace = 0)
 print(summary(nuevo.modelo))
 
@@ -94,6 +110,18 @@ preds_p <- factor(preds_e, levels = levels(datos[["EN"]]))
 
 ROC_p <- roc(muestra.prueba[["EN"]], prob_p)
 plot(ROC_p)
+
+
+# ------ ANÁLISIS DEL MODELO ------
+
+
+# La curva de ROC tanto para el conjunto de entrenamiento
+# como el conjunto de prueba, son alejados de la diagonal
+# pero presentan acercamientos en ciertos sectores.
+
+
+# ------ EVALUACIÓN DEL NUEVO MODELO ------
+
 
 #Obtener los residuos y las estadísticas
 # Obtener los residuos y las estadísticas .
@@ -156,7 +184,7 @@ cat("\n\n")
 print(output[sospechosos , ])
 
 
-#Verificar Condiciones
+# ------ VERIFICACIÓN DE LAS CONDICIONES ------
 
 
 # Verificación de multicolinealidad .
@@ -167,19 +195,17 @@ vifs <- vif (nuevo.modelo)
 print ( vifs )
 cat ("\n Promedio VIF: ")
 print ( mean ( vifs ) )
-# Si miramos los factores de inflación de la varianza, 
-# en general no parecen ser preocupantes, por lo que se verifica
-# la condición de multicolinealidad. 
+# En base a los valores VIF obtenidos
+# todos son menores a 10 por lo que lo que descartan los valores sesgados
 
 # Independencia de los residuos.
 cat (" Verificación de independencia de los residuos \n")
 cat (" - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - -- - - - - -\n")
 print(durbinWatsonTest(nuevo.modelo) )
+# Al obtener el valor p = 0.608, lo cual es mayor a nuestro alfa
+# por lo que con un 95% de confianza los residuos son independietnes
 
-
-#Evaluar poder predictivo
-
-
+# ----------- EVALUAR PODER PREDICTIVO DEL NUEVO MODELO -------
 prob_n <- predict(nuevo.modelo, muestra.entrenamiento, type = "response")
 
 preds_n <- sapply(prob_n, function(p) ifelse(p >= umbral, "1", "0"))
@@ -188,5 +214,24 @@ preds_n <- factor(preds_n, levels = levels(datos[["EN"]]))
 ROC_n <- roc(muestra.entrenamiento[["EN"]], prob_n)
 plot(ROC_n)
 
+# Al observar los gráficos de la curva de ROC para los grupos 
+# de entrenamiento y de prueba, éstas se van alejando de la 
+# diagonal, lo que indica una mejora en el modelo
 
+#----------------------------- COMPARANDO LOS MODELOS -------------------------------------- 
 
+# Hipótesis a contrastar                  
+
+# H0: Ambos modelos son iguales
+# HA: Un modelo es mejor que otra
+
+# Comparar los modelos RLS y el RLM
+cat ("\n\n")
+cat (" Likelihood Ratio Test para los modelos \n")
+cat (" - - - - - - - - - - -- - - - - - - - - - - - - - - - - - - - -- - - - - -\n")
+print (anova(modelo, nuevo.modelo, test = "LRT") )
+
+# Observando los resultados obtenidos al comparar los dos modelos,
+# se obtiene un p = 0.00012, valor muy significativo en comparación
+# al alfa definido (0.05), con ello podemos concluir con 95% de confianza
+# que el modelo con 4 predictores (el modelo nuevo) es mejor que modelo nulo.s
